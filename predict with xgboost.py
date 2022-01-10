2e9a7932e299d8795c9829b0c0b7495446d836da
shalev5611 <39675040+shalev5611@users.noreply.github.com>
2022-01-10 19:56:54 +0200

    Update predict with xgboost.py

--- predict with xgboost.py
+++ predict with xgboost.py
@@ -9,15 +9,13 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--path", required=True,
 	help="file path")
-ap.add_argument("-n", "--name", required=True,
-	help="file name")
 
 args = vars(ap.parse_args())
 print(args)
 
 
 model = xgboost.XGBClassifier()
-model.load_model("model.json")
+model.load_model("lies.json")
 
 rows = ['frame', ' face_id', ' timestamp', ' AU01_r', ' AU02_r', ' AU04_r', ' AU05_r', ' AU06_r', ' AU07_r', ' AU09_r', ' AU10_r', ' AU12_r', ' AU14_r', ' AU15_r', ' AU17_r', ' AU20_r', ' AU23_r', ' AU25_r', ' AU26_r', ' AU45_r', ' AU01_c', ' AU02_c', ' AU04_c', ' AU05_c', ' AU06_c', ' AU07_c', ' AU09_c', ' AU10_c', ' AU12_c', ' AU14_c', ' AU15_c', ' AU17_c', ' AU20_c', ' AU23_c', ' AU25_c', ' AU26_c', ' AU28_c', ' AU45_c']
 au = [' AU01_c', ' AU02_c', ' AU04_c', ' AU05_c', ' AU06_c', ' AU07_c', ' AU09_c', ' AU10_c', ' AU12_c', ' AU14_c', ' AU15_c', ' AU17_c', ' AU20_c', ' AU23_c', ' AU25_c', ' AU26_c', ' AU28_c', ' AU45_c']
@@ -28,6 +26,7 @@ def most_frequent(List):
     return max(set(List), key = List.count)
 def average(List):
     return sum(List)/len(List)
+
 
 def get_csv_from_docker(path, name):
     command = """sudo python3 -m http.server 8000 & nc -l -p 1234 > {}.csv & sudo docker run -it algebr/openface:latest -c "wget http://172.17.0.1:8000/{}.mp4; build/bin/FeatureExtraction -aus -f {}.mp4;ls processed; ifconfig; nc -w 3 172.17.0.1 1234 < processed/{}.csv";"""
@@ -76,13 +75,15 @@ def prepare_file(path):
     print(en)
     return np.array(en)
 
-def predict(model, path,name):
+def predict(model, path):
+    name = path.split(os.sep)[-1].partition(".")[0]
+    path = path.partition(".")[0]
+    print(name,path)
     get_csv_from_docker(path,name)
     #print(args["name"] == "trial_lie_011")
     data = prepare_file(str(name) + ".csv")
     pred = model.predict(np.array([data]))
     return pred
 
-pred = predict(model,args["path"],args["name"])
-
+pred = predict(model,args["path"])
 print("lie" if pred == 0 else "truth")
